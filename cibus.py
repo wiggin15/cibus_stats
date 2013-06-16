65b45f3606a055e59da972c2d885e36363992033
Arnon Yaari <arnony@infinidat.com>
2013-06-16 21:49:05 +0300

    change variable names

--- cibus.py
+++ cibus.py
@@ -4,18 +4,18 @@ import email
 # prefix = "ishur iska al sach -"
 prefix = '\xd7\x90\xd7\x99\xd7\xa9\xd7\x95\xd7\xa8 \xd7\xa2\xd7\xa1\xd7\xa7\xd7\x94 \xd7\xa2\xd7\x9c \xd7\xa1\xd7\x9a -'
 
-def get_mails_from(username, passw, fromwho):
+def get_mails_from(email_address, password, fromwho):
     mailbox = imaplib.IMAP4_SSL('imap.gmail.com')
-    mailbox.login(username, passw)
+    mailbox.login(email_address, password)
     mailbox.select('"[Gmail]/All Mail"')
-    
+
     result, data = mailbox.search(None, '(FROM "{}")'.format(fromwho))
-    
+
     for mail_id in data[0].split():
         status, data = mailbox.fetch(mail_id, "(RFC822)")
         mail = email.message_from_string(data[0][1])
         yield mail
-        
+
 def get_cibus_subjects(mails):
     for mail in mails:
         subject = email.Header.decode_header(mail['subject'])[0][0]
@@ -28,7 +28,7 @@ def parse_subjects(subjects):
         place, nis = subject.split(' NIS ')
         place = ' - '.join([part.strip() for part in place.split('-')])
         yield place, nis
-        
+
 def group_by_places(items):
     prices = dict()
     for place, nis in items:
@@ -39,12 +39,12 @@ def group_by_places(items):
 
 def main():
     import sys
-    _, username, passw = sys.argv
-    mails = get_mails_from(username, passw, "Cibus")
+    _, email_address, password = sys.argv
+    mails = get_mails_from(email_address, password, "Cibus")
     subjects = get_cibus_subjects(mails)
     items = parse_subjects(subjects)
     items = sorted(group_by_places(items), key=lambda x: x[1])
     open("cibus.txt", "wb").writelines(['{} {:.2f} {}\n'.format(*item) for item in items])
-    
+
 if __name__ == '__main__':
     main()
